3c7f430388ba294d8098ed55ae24c8a8d6995e9c
Marcin Hołubowicz <m.holubowicz87@gmail.com>
2021-04-24 22:56:32 +0200

    Basic structure

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+const { render } = require('node-sass');
+
+{
+  ('use strict');
+
+  const select = {
+    templateOf: {
+      book: '#template-book',
+    },
+    containerOf: {
+      booksList: '.books-list',
+    },
+  };
+    function render() {
+      const thisBooksList = this;
+    }
+  }
+render();
+}
